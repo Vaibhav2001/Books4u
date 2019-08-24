0c40349c6924ed2f694ad533f4a709b80a754f35
Vaibhav Agrawal <vaibhav.ag.001@gmail.com>
2019-08-24 13:07:07 +0530

    still debigging

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
+//DB Config
+const db = require('../config/keys').MongoURI;
+
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const User = mongoose.model('User');
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard', {
         user: req.user
     })
-);
+});
 
 router.post('/dashboard', ensureAuthenticated, (req, res) => {
     req.flash('success_msg', 'Book added, Reload to view the changes');
     res.redirect('dashboard')
 });
-//Remove
-router.get('/remove', ensureAuthenticated, (req, res) =>
-    res.render('remove', {
-        user: req.user
-    })
-);
+
+// Add
+router.post('/add', ensureAuthenticated, (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+    console.log(req.user.books);
+    const newBooks = req.user.books;
+    newBooks.push(name);
+    console.log(newBooks);
+    User.updateOne(
+        {"email" : req.user.email},
+        {$set: { "books" : newBooks}});
+    console.log(req.user.books);
+    req.flash('success_msg', 'Book added, Reload to view the changes');
+    // res.render('dashboard', {
+    //     user: req.user
+    // });
+});
+
+
 
 module.exports = router;
